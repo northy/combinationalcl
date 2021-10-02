355fd71c9aae3cbdfb684d1b1dff2e5ad7a78d4a
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2021-10-02 15:07:52 -0300

    Improved error handling on collect script

--- exec/collect.py
+++ exec/collect.py
@@ -2,12 +2,19 @@
 import subprocess as sp
 import itertools
 import platform
+import signal
 
 ##### Definitions #####
 dummy = False
-limit_P = 10000
-K = 10000
+limit_P = 500
+K = 1000
 #######################
+
+def signal_handler(sig, frame):
+    print('\nExiting...')
+    exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 def doit(p,i,o) :
     print(f"Running with {p} ports, {i} inputs, {o} outputs (i+o+p={i+o+p})...")
@@ -23,14 +30,36 @@ def doit(p,i,o) :
     print("Done!")
     return errstr, rc
 
-def check_return(x) :
-    if x[1] : print(f"An error occured, aborting...",x[0],sep='\n'); exit(1)
+limit_other = limit_P+1
+
+def log_noexec(error,p,i,o) :
+    with open("log_error.txt",'a') as f :
+        f.writelines(['P: '+str(p)+', I: '+str(i)+', O: '+str(o)+"\n",str(error), '\n-----\n\n'])
+
+def check_return(x,p,i,o) :
+    global limit_other
+
+    if x[1] :
+        print(f"An error occured...",sep='\n')
+        log_noexec(x[0],p,i,o)
+        limit_other = max(i,o)
 
 I_O_count = itertools.count(10,10)
 P_count = itertools.count(100,100)
 
 for I_O_max,P in zip(I_O_count,P_count) :
-    if P>limit_P : break
+    if P>limit_P or limit_other<=limit_P : break
     for I,O in itertools.combinations_with_replacement(range(10,I_O_max+1,10), 2) :
-        check_return(doit(P,I,O))
-        if I!=O : check_return(doit(P,O,I))
+        #this tries to prevent executions that would
+        #most likely fail
+        if O>=limit_other or I>=limit_other :
+            log_noexec('skipping',P,I,O)
+            if I!=O : log_noexec('skipping',P,O,I)
+            continue
+        check_return(doit(P,I,O), P,I,O)
+        if I!=O :
+            #if last execution failed, this will probably also fail
+            if O>=limit_other or I>=limit_other :
+                log_noexec('skipping',P,O,I)
+                continue
+            check_return(doit(P,O,I), P,O,I)
